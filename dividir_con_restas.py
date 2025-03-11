286eeb3144eb47b095f6b3debdbfb995a1bb6881
jpepinus <pablo.castillo@iteso.mx>
2025-03-10 19:12:00 -0600

    Code solution

--- dividir_con_restas.py
+++ dividir_con_restas.py
@@ -1,15 +1,24 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Juan Pablo Castillo Haros 
 """
 
 # Declaraciones
-CONSTANTE = valor
+dividendo = float(input("Introduze el dividendo:")) 
+divisor = float(input("Introduze el divisor:"))
+contador = 0
+residuo = dividendo  
 
-# Entradas
-entrada = input()
-
-# Proceso
-
+#Entradas
+if divisor == 0:
+    print("Error: El divisor no puede ser cero.")
+else:
+    while True:
+        if divisor <= dividendo:
+            dividendo = dividendo - divisor  
+            contador += 1
+        else:
+            residuo = dividendo  
+            break
 
 # Salidas
-print(salida)
+print("El cociente es:", contador ,"y el Residuo es:", residuo)
